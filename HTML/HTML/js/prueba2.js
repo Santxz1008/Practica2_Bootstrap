0a67ea374092536944815b9944fcee1dcc833709
Santxz1008 <105953498+Santxz1008@users.noreply.github.com>
2022-07-01 08:10:18 -0500

    Delete HTML/HTML directory

--- HTML/HTML/js/prueba2.js
+++
@@ -1,8 +0,0 @@
-function validarFormulario(){
-    let x = document.forms["frml"]["Nombre"].value; 
-    if(x==="")
-    {
-        alert("Se debe completar el nombre"); 
-        return false;
-    }
-}
